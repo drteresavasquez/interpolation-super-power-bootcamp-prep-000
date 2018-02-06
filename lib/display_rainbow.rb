1e6861cc1ecfda86d13b9874f781c4d0aa463f60
Teresa Terry <dr.terry2013@gmail.com>
2018-02-06 18:47:53 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(arr)
-  
+  puts "#{arr[0][0].upcase}: #{arr[0]}"
 end
 
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
